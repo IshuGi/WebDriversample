e93c38fa293bd0a3aebbc162d3982b708d30cd9d
Administrator <Administrator@172.31.14.138>
2023-07-18 08:00:23 +0000

    updated

--- src/test/java/ParallelExecution/Sampletwo.java
+++ src/test/java/ParallelExecution/Sampletwo.java
@@ -3,7 +3,7 @@ package ParallelExecution;
 import org.testng.annotations.Test;
 
 public class Sampletwo {
-	  @Test
+	  @Test(invocationCount=6,threadPoolSize= 3, timeOut=2000)
 	  public void five() {
 		  System.out.println("five");
 	  }
